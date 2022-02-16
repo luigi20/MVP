a6f54287d08112f31f027aa5dd7fffd19158f1e0
Luis <luisopentec@gmail.com>
2022-02-16 18:41:46 -0300

    model and migration user add

---
+++ test/integration/session.spec.ts
@@ -0,0 +1,9 @@
+import User from 'App/Models/User';
+import test from 'japa';
+
+test('it should return JWT token when session created', async () => {
+  const user = await User.create({
+
+  });
+});
+
